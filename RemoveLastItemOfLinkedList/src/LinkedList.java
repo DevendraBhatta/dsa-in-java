e3f9b56475e6f13233409de497c651ae5daba91c
DevendraBhatta <devendrabhatta63@gmail.com>
2025-07-15 14:32:12 +0545

    linked list in dsa

--- RemoveLastItemOfLinkedList/src/LinkedList.java
+++ RemoveLastItemOfLinkedList/src/LinkedList.java
@@ -1,28 +1,27 @@
-public class LinkedList {
 
+
+public class LinkedList {
     private Node head;
     private Node tail;
     private int length;
 
-    // Node Class
+
     public static class Node {
         int value;
         Node next;
 
-        // Node Class's Constructor
         Node(int value) {
             this.value = value;
         }
     }
 
-
-    // LinkedList Class's Constructor
     public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
+
 
     // printList function
     public void printList() {
@@ -44,5 +43,38 @@ public class LinkedList {
 
     public void getLength() {
         System.out.println("Length:" + length);
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
     }
 }
---
+++ RemoveLastItemOfLinkedList/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast());
+    }
+
+}
